e3b3204ff176eae5a6ac1897871aefc7f5c1dfad
schwartzbacher <schwartzbacher@techie.com>
2017-12-08 14:08:22 +0100

    Update ad.js

--- js/ad.js
+++ js/ad.js
@@ -1,1 +1,19 @@
-confirm("Do you wish to take surveys and get paid in USD?");
+mscConfirm({
+  title: 'Sponsored Ad',
+
+  subtitle: 'Would you like to make extra income by taking surveys?',  // default: ''
+
+  okText: 'Yes',    // default: OK
+
+  cancelText: 'I Dont', // default: Cancel,
+
+  dismissOverlay: true, // default: false, closes dialog box when clicked on overlay.
+
+  onOk: function() {
+    alert('Awesome.');
+  },
+
+  onCancel: function() {
+    alert('Sad face :( .');
+  }
+});
